418102c4bd3438c0b0530c7ec7d5422ea00d731c
famzyjr <famuyiwaahmed132@gmail.com>
2025-08-08 00:55:29 +0100

    sevvent-commit

--- app.js
+++ app.js
@@ -1,6 +1,51 @@
-const ApiKey = '1966e9143c28d8e33a6fe2ddf564d571';
+const Results = document.getElementById('result');
+const Search = document.getElementById('searchinput')
+const MovieDetails = document.getElementById('movieDetails');
+const First = document.getElementById('firstSet');
+const LoadingIndicator = document.getElementById('loadingIndicator');
 const FirstSet = document.getElementById('firstSet');
-const loAdingindicator = document.getElementById('loAdingindicator');
+
+const ApiKey = '677cef6d';
+function setActive(e) {
+  const buttons = document.querySelectorAll('.button');
+  buttons.forEach(button => {
+    button
+      .classList.remove('active');
+  }
+  )
+  e.classList.add('active')
+}
+Search.addEventListener('input', async () => {
+  const query = Search.value.trim();
+  LoadingIndicator.style.display = 'block';
+  if (query.length < 3) {
+    Results.innerHTML = '';
+    return;
+  }
+  try {
+    const response = await fetch(`https://www.omdbapi.com/?s=${query}&apikey=${ApiKey}`);
+    const data = await response.json();
+    if (data.Search) {
+      Results.innerHTML = data.Search.map(items => (`
+<div class='result-item data-id=${data.imbdID}'>
+<img  src="${items.Poster}" />(${items.Year})
+<div>${items.Title}</div>
+
+</div>
+    `)).join('')
+    } else {
+      let NotFound = 'Results Not Found';
+      Results.innerHTML = `<div class='not'>${NotFound}<div>`
+    }
+  } catch (err) {
+    err.log
+  } finally {
+    LoadingIndicator.style.display = 'block'
+  }
+
+})
+
+
 
 function goToDetails(id) {
   window.location.href = `movie.html?id=${id}`;
@@ -17,12 +62,16 @@ function bookmarkMovie(id, title, posterPath) {
   localStorage.setItem('bookmarkedMovies', JSON.stringify(saved));
   alert(`${title} bookmarked!`);
 }
+const loAdingindicator = document.getElementById('loAdingindicator')
+let ApIkeys = '1966e9143c28d8e33a6fe2ddf564d571';
 
 const Discover = async () => {
   // loAdingindicator.style.display = 'block';
   try {
-    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${ApiKey}`);
+    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${ApIkeys}`);
     const data = await response.json();
+    console.log(data);
+    
     let html = '';
 
     for (let movie of data.results) {
@@ -41,10 +90,10 @@ const Discover = async () => {
   } catch (error) {
     console.error(error);
   } finally {
-    loAdingindicator.style.display = 'none';
+    // loAdingindicator.style.display = 'none';
   }
 };
 
 setTimeout(() => {
-   Discover()
+  Discover() 
 }, 3000);
